e7d5a1be4c0b4a51e63b9de68ba294c3ae680c07
ofadiman <ofadiman@gmail.com>
2024-03-02 14:59:56 +0100

    chore: delete typescript directory

--- typescript/src/@types/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { Server } from 'socket.io'
-import { SocketData, InterServerEvents, ServerToClientEvents, ClientToServerEvents } from '../types'
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    io: Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>
-  }
-}
